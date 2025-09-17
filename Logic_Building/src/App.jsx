cc76a704aa911d125439f8613eea542bf0445c94
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-17 20:04:04 +0530

    App

--- Logic_Building/src/App.jsx
+++ Logic_Building/src/App.jsx
@@ -3,8 +3,10 @@ import ContextAPI from './components/ContextAPI'
 import ControlledComponents from './components/ControlledComponents'
 import UseCustomHook from './components/Custom Hooks/UseCustomHook'
 import HigherOrderFunction from './components/HigherOrderFunction'
+import Lazyloading from './components/Lazy loading/Lazyloading'
 import PropDrilling from './components/PropDrilling'
 import UseEffectHook from './components/UseEffectHook'
+import UseMemoHook from './components/UseMemoHook'
 import UseStateHook from './components/UseStateHook'
 import VirtualDom from './components/VirtualDom'
 
@@ -20,7 +22,10 @@ function App() {
       {/* <PropDrilling></PropDrilling> */}
       {/* <ContextAPI></ContextAPI> */}
       {/* <HigherOrderFunction></HigherOrderFunction> */}
-      <UseCustomHook></UseCustomHook>
+      {/* <UseCustomHook></UseCustomHook> */}
+      {/* <Lazyloading></Lazyloading> */}
+      {/* <UseMemoHook></UseMemoHook> */}
+
     </>
   )
 }
